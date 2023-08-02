983917f34bb38e2ed4607d6f4fa35254bd2c0bef
syckor <syckor@naver.com>
2023-08-02 23:46:55 +0900

    h2 database 및 기본 설정

--- src/main/java/com/example/jpashop/HelloController.java
+++ src/main/java/com/example/jpashop/HelloController.java
@@ -1,11 +1,17 @@
 package com.example.jpashop;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class HelloController {
+
+    @Autowired
+    MemberRepository memberRepository;
 
     @GetMapping("hello")
     public String hello(Model model){
