1495c48fee106772056a3a1ab0210f8b2d6a4892
syckor <syckor@naver.com>
2023-08-07 22:07:04 +0900

    모든 XtoOne 연관관계의 Fetch는 Lazy로 변경

--- src/main/java/com/example/jpashop/domain/Address.java
+++ src/main/java/com/example/jpashop/domain/Address.java
@@ -10,4 +10,13 @@ public class Address {
     private String city;
     private String street;
     private String zipcode;
+
+    protected Address(){}
+
+    //처음 객체 생성될 때만 값을 넣고 변경 불가능하게 만듬
+    public Address(String city, String street, String zipcode) {
+        this.city = city;
+        this.street = street;
+        this.zipcode = zipcode;
+    }
 }
