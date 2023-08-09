960dbf71a7443ad4d356e24fbe5934ce73549f04
syckor <syckor@naver.com>
2023-08-09 22:46:59 +0900

    MemberRepository에서 생성자 DI 적용

--- src/main/java/com/example/jpashop/repository/MemberRepository.java
+++ src/main/java/com/example/jpashop/repository/MemberRepository.java
@@ -3,15 +3,17 @@ package com.example.jpashop.repository;
 import com.example.jpashop.domain.Member;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 @Repository
+@RequiredArgsConstructor
 public class MemberRepository {
 
-    @PersistenceContext
-    private EntityManager em;
+    private final EntityManager em;
 
     public void save(Member member){
         em.persist(member);
