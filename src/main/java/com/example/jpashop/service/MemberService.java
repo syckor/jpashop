5a4f51d29f820989ec1c7dd252a3b13ebfdf4e10
syckor <syckor@naver.com>
2023-08-09 22:44:44 +0900

    MemberService 구현

---
+++ src/main/java/com/example/jpashop/service/MemberService.java
@@ -0,0 +1,41 @@
+package com.example.jpashop.service;
+
+import com.example.jpashop.domain.Member;
+import com.example.jpashop.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Service
+public class MemberService {
+
+    private final MemberRepository memberRepository;
+
+    //회원 가입
+    @Transactional
+    public Long join(Member member){
+        validateDuplicateMember(member); //중복 회원 검증
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member) {
+        //Exception
+        List<Member> findMembmers = memberRepository.findByName(member.getName());
+        if(!findMembmers.isEmpty()) throw new IllegalStateException("이미 존재하는 회원");
+    }
+
+    //회원 전체 조회
+    @Transactional(readOnly = true)
+    public List<Member> findMembers(){
+        return memberRepository.findAll();
+    }
+
+    @Transactional(readOnly = true)
+    public Member findOne(Long memberId){
+        return memberRepository.findOne(memberId);
+    }
+}
