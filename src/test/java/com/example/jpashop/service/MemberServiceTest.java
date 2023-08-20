51837a09392c15b876c4c98979cac28e4e00acc3
syckor <syckor@naver.com>
2023-08-20 23:09:31 +0900

    MemberService 구현한거 test class 작성 (중복회원예외, 회원가입)

---
+++ src/test/java/com/example/jpashop/service/MemberServiceTest.java
@@ -0,0 +1,59 @@
+package com.example.jpashop.service;
+
+import com.example.jpashop.domain.Member;
+import com.example.jpashop.repository.MemberRepository;
+import jakarta.persistence.EntityManager;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.junit.Assert.*;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@Transactional
+public class MemberServiceTest {
+
+    @Autowired MemberService memberService;
+    @Autowired MemberRepository memberRepository;
+    @Autowired
+    EntityManager em; //이 에러는 제외
+
+    @Test
+    @Rollback(false)
+    void 회원가입() throws Exception{
+        //given
+        Member member = new Member();
+        member.setName("kim");
+
+        //when
+        Long savedId = memberService.join(member);
+        //then
+        em.flush();
+        assertEquals(member, memberRepository.findOne(savedId));
+    }
+
+    @Test
+    public void 중복_회원_예외(){
+        //given
+        Member member1 = new Member();
+        member1.setName("kim1");
+
+        Member member2 = new Member();
+        member2.setName("kim1");
+        //when
+        memberService.join(member1);
+
+
+
+        //then
+        Assertions.assertThrows(IllegalStateException.class, () -> {
+            memberService.join(member2);
+        });
+    }
+}
