5e9a81aa8a3a127cf4f2ff051ee4148c9dd105b6
syckor <syckor@naver.com>
2023-08-22 22:32:39 +0900

    OrderRepository develop

---
+++ src/main/java/com/example/jpashop/repository/OrderRepository.java
@@ -0,0 +1,24 @@
+package com.example.jpashop.repository;
+
+import com.example.jpashop.domain.Order;
+import jakarta.persistence.EntityManager;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+@RequiredArgsConstructor
+public class OrderRepository {
+    private final EntityManager em;
+
+    public void save(Order order){
+        em.persist(order);
+    }
+
+    public Order findOne(Long id){
+        return em.find(Order.class, id);
+    }
+
+    //ToDo public List<Order> findAll(OrderSearch orderSearch){}
+}
