fd9fd720b6837bda0421566b5c3bd7571901ddce
syckor <syckor@naver.com>
2023-08-23 23:36:46 +0900

    OrderService orderSearch dynamic sql by jpql develop

--- src/main/java/com/example/jpashop/repository/OrderRepository.java
+++ src/main/java/com/example/jpashop/repository/OrderRepository.java
@@ -5,6 +5,8 @@ import jakarta.persistence.EntityManager;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 @RequiredArgsConstructor
 public class OrderRepository {
@@ -18,5 +20,16 @@ public class OrderRepository {
         return em.find(Order.class, id);
     }
 
-    //ToDo public List<Order> findAll(OrderSearch orderSearch){}
+    public List<Order> findAll(OrderSearch orderSearch){
+        String jpql = "select o from Order o join o.member m";
+       return  em.createQuery(jpql +
+                "where o.status = :status" +
+                "and m.name like :name"
+                , Order.class)
+               .setParameter("status", orderSearch.getOrderStatus())
+               .setMaxResults(1000)
+                .getResultList();
+    }
+
+    //finally we need to use queryDsl
 }
